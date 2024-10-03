650737b80cea53b2af80634c72e6c2fae4ea87d1
asmtv1 <52816913+asmtv1@users.noreply.github.com>
2024-10-03 11:37:55 +0400

    Update TicketFilter.jsx
    Избавился от таблицы. теперь это ul с li

--- src/components/TicketFilter/TicketFilter.jsx
+++ src/components/TicketFilter/TicketFilter.jsx
@@ -10,60 +10,56 @@ export default function TicketFilter() {
   // Изменено имя компонента
   const dispatch = useDispatch();
   const selectedFilter = useSelector(
-    (state) => state.ticketFilter.selectedTicketFilter,
+    (state) => state.ticketFilter.selectedTicketFilter
   );
 
   return (
-    <table className="ticket-filter">
-      <tbody>
-        <tr>
-          <td>
-            <input
-              className="ticket-filter__button"
-              type="radio"
-              id="cheapest"
-              name="ticket_filter"
-              onChange={() => dispatch(setCheapest())}
-              checked={selectedFilter === "cheapest"}
-            />
-            <label
-              className="ticket-filter__label ticket-filter__label--first"
-              htmlFor="cheapest"
-            >
-              САМЫЙ ДЕШЕВЫЙ
-            </label>
-          </td>
-          <td>
-            <input
-              className="ticket-filter__button"
-              type="radio"
-              id="fastest"
-              name="ticket_filter"
-              onChange={() => dispatch(setFastest())}
-              checked={selectedFilter === "fastest"}
-            />
-            <label className="ticket-filter__label" htmlFor="fastest">
-              САМЫЙ БЫСТРЫЙ
-            </label>
-          </td>
-          <td>
-            <input
-              className="ticket-filter__button"
-              type="radio"
-              id="optimal"
-              name="ticket_filter"
-              onChange={() => dispatch(setOptimal())}
-              checked={selectedFilter === "optimal"}
-            />
-            <label
-              className="ticket-filter__label ticket-filter__label--last"
-              htmlFor="optimal"
-            >
-              ОПТИМАЛЬНЫЙ
-            </label>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <ul className="ticket-filter">
+      <li>
+        <input
+          className="ticket-filter__button"
+          type="radio"
+          id="cheapest"
+          name="ticket_filter"
+          onChange={() => dispatch(setCheapest())}
+          checked={selectedFilter === "cheapest"}
+        />
+        <label
+          className="ticket-filter__label ticket-filter__label--first"
+          htmlFor="cheapest"
+        >
+          САМЫЙ ДЕШЕВЫЙ
+        </label>
+      </li>
+      <li>
+        <input
+          className="ticket-filter__button"
+          type="radio"
+          id="fastest"
+          name="ticket_filter"
+          onChange={() => dispatch(setFastest())}
+          checked={selectedFilter === "fastest"}
+        />
+        <label className="ticket-filter__label" htmlFor="fastest">
+          САМЫЙ БЫСТРЫЙ
+        </label>
+      </li>
+      <li>
+        <input
+          className="ticket-filter__button"
+          type="radio"
+          id="optimal"
+          name="ticket_filter"
+          onChange={() => dispatch(setOptimal())}
+          checked={selectedFilter === "optimal"}
+        />
+        <label
+          className="ticket-filter__label ticket-filter__label--last"
+          htmlFor="optimal"
+        >
+          ОПТИМАЛЬНЫЙ
+        </label>
+      </li>
+    </ul>
   );
 }
